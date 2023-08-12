187d6c8c8a49f59ba48366a463fd4270dca1c540
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-12 11:14:07 +0330

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek5.Power_Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PowerMethod
+
+            Console.WriteLine("Enter base number: ");
+            double myBaseNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter power number: ");
+            double myPowerNumber = Convert.ToDouble(Console.ReadLine());
+
+            var myResult = Power(myBaseNumber, myPowerNumber);
+            Console.WriteLine($"The result is: {myResult}");
+        }
+
+        static double Power(double baseNum, double powerNum)
+        {
+            double result = 1;
+            if (powerNum > 0)
+            {
+                for (int i = 1; i <= powerNum; ++i)
+                {
+                    result *= baseNum;
+                }
+            }
+            else if (powerNum < 0)
+            {
+                for (int i = -1; i >= powerNum; --i)
+                {
+                    result /= baseNum;
+                }
+            }
+            return result;
+        }
+    }
+}
